67c891c7446805e36afedb1bd415872c3a5ac138
Avishka Lakshan <avishka.2019193@iit.ac.lk>
2026-02-01 04:34:50 +0200

    Exercise 1.1 - 1.5
    Exercises including 1.1 - 1.5

---
+++ part1/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/Avishka2019193'>Avishka</a>
+    </div>
+  )
+}
+
+const App = () => {
+
+  
+
+  return (
+    <>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Footer />
+    </>
+  )
+}
+
+export default App
